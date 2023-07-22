11f74512962522c958ee930ae26dcc65d340dfb6
Yeison Esteban Pretel Puentes <43614257+yeprepue@users.noreply.github.com>
2023-07-22 00:03:33 -0500

    Se agregó localStorage y se estilizaron iconos.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,53 @@
-import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react'; import { TodoCounter }
+  from './TodoCounter'; import { TodoSearch }
+  from './TodoSearch'; import { TodoList }
+  from './TodoList'; import { TodoItem }
+  from './TodoItem'; import { CreateTodoButton }
+  from './CreateTodoButton';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el Curso de Intro a React.js', completed: false },
-  { text: 'Llorar con la Llorona', completed: false },
-  { text: 'LALALALALA', completed: false },
-  { text: 'Usar estados derivados', completed: true },
-];
+//   const defaultTodos = [
+//     { text: 'Cortar cebolla', completed: true },
+//     { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//     { text: 'Llorar con la Llorona', completed: false },
+//     { text: 'LALALALALA', completed: false },
+//     { text: 'Usar estados derivados', completed: true },
+//   ];
+//  localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+  const [item, setItem] = React.useState(parsedItem);
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  };
+  return [item, saveItem];
+}
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
-  const [searchValue, setSearcValue] = React.useState('');
-
-  const completedTodos = todos.filter(
-    todo => !!todo.completed
-  ).length;
-
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const [searchValue, setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
-
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLocaleLowerCase();
-      const searchText = searchValue.toLocaleLowerCase();
-      return todoText.includes(searchText);
-    }
-  );
-  const completeTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
-    );
-    newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
-  };
-  const deleteTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
-    );
-    newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
-  };
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+  const completeTodo = (text) => { const newTodos = [...todos]; const todoIndex = newTodos.findIndex((todo) => todo.text == text); newTodos[todoIndex].completed = true; saveTodos(newTodos); }; const deleteTodo = (text) => { const newTodos = [...todos]; const todoIndex = newTodos.findIndex((todo) => todo.text == text); newTodos.splice(todoIndex, 1); saveTodos(newTodos); };
   return (
     <>
       <TodoCounter
         completed={completedTodos}
-        total={totalTodos}
-      />
-      <TodoSearch
+        total={totalTodos} /><TodoSearch
         searchValue={searchValue}
-        setSearcValue={setSearcValue}
-      />
-
+        setSearchValue={setSearchValue} />
       <TodoList>
         {searchedTodos.map(todo => (
           <TodoItem
@@ -65,14 +56,11 @@ function App() {
             completed={todo.completed}
             onComplete={() => completeTodo(todo.text)}
             onDelete={() => deleteTodo(todo.text)}
-
           />
         ))}
       </TodoList>
-
       <CreateTodoButton />
     </>
   );
 }
-
-export default App; 
+export default App;
